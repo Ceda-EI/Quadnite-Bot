8274ab09805fd07b08d5053203d7b29c2c004ef3
Sphericalkat <me@kat.bio>
2024-09-30 10:39:07 +0530

    feat: add rate limiter middleware
    Signed-off-by: Sphericalkat <me@kat.bio>

--- bot.js
+++ bot.js
@@ -1,12 +1,20 @@
 const { Telegraf } = require("telegraf");
-const { BOT_API_KEY, FEEDBACK_ID, UGOKI_ROOT } = process.env;
+const { BOT_API_KEY, FEEDBACK_ID, UGOKI_ROOT, RATE_TIMEFRAME, RATE_LIMIT } = process.env;
 const fs = require("fs").promises;
 const commands = require("./commands");
 const axios = require("axios");
 const roleplay = require("./static/roleplay.json");
+const { limit } = require("@grammyjs/ratelimiter");
 
 const bot = new Telegraf(BOT_API_KEY);
 bot.catch((err) => console.log(err));
+bot.use(limit({
+    // default config: 5 messages per 5 seconds
+    timeFrame: RATE_TIMEFRAME ?? 5000,
+    limit: RATE_LIMIT ?? 5,
+
+    onLimitExceeded: (ctx, next) => ctx.reply('Too many requests!'),
+}))
 
 const data = [
 	"questions",
